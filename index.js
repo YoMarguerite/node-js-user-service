6178a26dbcd7e2c683e06b403f6685c757cfba3d
Yoann MARGUERITE <yoann.marguerite@epsi.fr>
2020-01-14 10:26:15 +0100

    ajout de la doc et gestion d'erreur

--- index.js
+++ index.js
@@ -3,8 +3,38 @@ var bodyParser = require('body-parser');
 var sql = require("./mysql.js");
 
 var app = express();
+const expressSwagger = require('express-swagger-generator')(app);
 var hostname = 'localhost'; 
 var port = 3000; 
+
+let options = {
+    swaggerDefinition: {
+        info: {
+            description: 'Here you can find the documentation of this microservice',
+            title: 'Swagger',
+            version: '1.0.0',
+        },
+        host: 'localhost:3000',
+        basePath: '/v1',
+        produces: [
+            "application/json",
+            "application/xml"
+        ],
+        schemes: ['http', 'https'],
+        securityDefinitions: {
+            JWT: {
+                type: 'apiKey',
+                in: 'header',
+                name: 'Authorization',
+                description: "",
+            }
+        }
+    },
+    basedir: __dirname, //app absolute path
+    files: ['index.js'] //Path to the API handle folder
+};
+expressSwagger(options)
+
 
 app.use( bodyParser.json() );
 app.use(bodyParser.urlencoded({
@@ -14,11 +44,24 @@ app.use(bodyParser.urlencoded({
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
     next();
 });
 
 var myRouter = express.Router();
 
+/**
+ * This function comment is parsed by doctrine
+ * @route POST /login connect an user
+ * @group Login - Use for sign in
+ * @param {string} login - user's login
+ * @param {string} pwd - user's password
+ * @returns {object} 200 - return user's data
+ *                         |_ if login doesn't exist, return object with a message error 
+ *                         |_ if password is incorrect, return object with a message error
+ *                         |_ if login or password typeof !== 'string', return object with a message error
+ *                         |_ Unexpected error
+ */
 myRouter.route('/login')
     .post(function(req, res){
         try{
@@ -29,6 +72,18 @@ myRouter.route('/login')
     })
 
 myRouter.route('/user')
+
+    /**
+     * This function comment is parsed by doctrine
+     * @route POST /user create a new user
+     * @group User - Use to manage user account
+     * @param {string} login - user's login
+     * @param {string} pwd - user's password
+     * @param {string} mail - user's mail
+     * @returns {object} 200 - return user's id
+     *                         |_ if login already exist, return object with a message error
+     *                         |_ Unexpected error
+     */
     .post(function(req,res){
         try{
             sql.registerUser(req,res);
@@ -36,6 +91,19 @@ myRouter.route('/user')
             console.log(err)
         }
     })
+
+    /**
+     * This function comment is parsed by doctrine
+     * @route PUT /user modify an user
+     * @group User - Use to manage user account
+     * @param {string} id - user's id
+     * @param {string} login - user's login
+     * @param {string} pwd - user's password
+     * @param {string} mail - user's mail
+     * @returns {object} 200 - return object with number of affected rows
+     *                         |_ if login already exist, return object with a message error
+     *                         |_ Unexpected error
+     */
     .put(function(req, res){
         try{
             sql.modifUser(req, res);
@@ -43,6 +111,15 @@ myRouter.route('/user')
             console.log(err)
         }
     })
+
+    /**
+     * This function comment is parsed by doctrine
+     * @route DELETE /user delete an user
+     * @group User - Use to manage user account
+     * @param {string} id - user's id
+     * @returns {object} 200 - return object with number of affected rows
+     *                         |_ Unexpected error
+     */
     .delete(function(req,res){
         try{
             sql.deleteUser(req, res);
--- mysql.js
+++ mysql.js
@@ -17,67 +17,98 @@ const deleteUser = "DELETE FROM user WHERE id = ?;";
 var sql = {
 
     connectUser: function (req, res) {
-        let login = req.body.login;
-        let pwd = req.body.pwd;
-        con.query(getUserByLogin, [login], function (err, result) {
-            if (err) throw err;
-            if(result[0] != undefined){
-                if(passwordHash.verify(pwd, result[0].pwd)){
-                    res.json(result);
-                    sendMail.send("Connexion", {login:login, mail:result[0].mail});
-                }else{
-                    res.json({"error":"Le mot de passe est incorrect..."});
-                }
+        try{
+            let login = req.body.login;
+            let pwd = req.body.pwd;
+            if(typeof login === 'string' && typeof pwd === 'string'){
+                con.query(getUserByLogin, [login], function (err, result) {
+                    try{
+                        if(result[0] != undefined){
+                            if(passwordHash.verify(pwd, result[0].pwd)){
+                                res.json(result);
+                                sendMail.send("Connexion", {login:login, mail:result[0].mail});
+                            }else{
+                                res.json({error:"Le mot de passe est incorrect..."});
+                            }
+                        }else{
+                            res.json({error:"Le login n'existe pas..."})
+                        }
+                    }catch(err){
+                        res.json({error:err})
+                    }
+                });
             }else{
-                res.json({"error":"Le login n'existe pas..."})
+                res.json({error:"Les paramètres login et pwd sont invalides..."})
             }
-        });
+        }catch(err){
+            res.json({error:err})
+        }
     },
 
     registerUser: function (req, res) {
-      let login = req.body.login;
-      let mail = req.body.mail;
-      let pwd = req.body.pwd;
-      con.query(getUserByLogin, [login], function (err, result) {
-          if (err) throw err;
-          if(result[0] === undefined){
-              con.query(insertUser, [login, mail, passwordHash.generate(pwd)], function(err, result) {
-                  if (err) throw err;
-                  res.json({id:result.insertId});
-                  sendMail.send("Inscription", {login:login, mail:mail});
-              });
-          }else{
-              res.json({error:"Ce login est déjà utilisé..."});
-          }
-      });
+        try{
+            let login = req.body.login;
+            let mail = req.body.mail;
+            let pwd = req.body.pwd;
+            con.query(getUserByLogin, [login], function (err, result) {
+                try{
+                    if(result[0] === undefined){
+                        con.query(insertUser, [login, mail, passwordHash.generate(pwd)], function(err, result) {
+                            if (err) throw err;
+                            res.json({id:result.insertId});
+                            sendMail.send("Inscription", {login:login, mail:mail});
+                        });
+                    }else{
+                        res.json({error:"Ce login est déjà utilisé..."});
+                    }
+                }catch(err){
+                    res.json({error:err})
+                }
+            });
+        }catch(err){
+            res.json({error:err})
+        }
     },
 
     modifUser: function(req, res){
-      let id = req.body.id;
-      let login = req.body.login;
-      let mail = req.body.mail;
-      let pwd = req.body.pwd;
-      con.query(getUserByLogin, [login], function (err, result) {
-          if (err) throw err;
-          if ((result[0] === undefined)||(result[0].id == id)) {
-              con.query(updateUser, [login, mail, passwordHash.generate(pwd), id], function (err, result) {
-                  if (err) throw err;
-                  res.json(result);
-                  sendMail.send("Modification", {login:login, mail:mail});
-              });
-          } else {
-              res.json({error: "Ce login est déjà utilisé..."});
-          }
-      });
+        try{
+            let id = req.body.id;
+            let login = req.body.login;
+            let mail = req.body.mail;
+            let pwd = req.body.pwd;
+            con.query(getUserByLogin, [login], function (err, result) {
+                try{
+                    if ((result[0] === undefined)||(result[0].id == id)) {
+                        con.query(updateUser, [login, mail, passwordHash.generate(pwd), id], function (err, result) {
+                            if (err) throw err;
+                            res.json(result);
+                            sendMail.send("Modification", {login:login, mail:mail});
+                        });
+                    } else {
+                        res.json({error: "Ce login est déjà utilisé..."});
+                    }
+                }catch(err){
+                    res.json({error:err})
+                }
+            });
+        }catch(err){
+            res.json({error:err})
+        }
     },
 
     deleteUser: function(req, res){
-        let id = req.body.id;
-        con.query(deleteUser, [id], function(err, result) {
-            if (err) throw err;
-            res.json(result)
-            sendMail.send("Modification", {login:req.body.login, mail:req.body.mail});
-        });
+        try{
+            let id = req.body.id;
+            con.query(deleteUser, [id], function(err, result) {
+                try{
+                    res.json(result)
+                }catch(err){
+                    res.json({error:err})
+                }
+            });
+        }catch(err){
+            res.json({error:err})
+        }
     }
 }
 
